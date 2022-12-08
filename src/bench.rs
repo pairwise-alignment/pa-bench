f0cdf21df58a4086dff3306c7ad80085a8aee4ee
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-12-08 17:31:52 +0100

    Refactor with more types; add file IO

--- src/bench.rs
+++ src/bench.rs
@@ -1,45 +1,51 @@
-use std::time::Instant;
+use std::time::{Duration, Instant};
 
+use core_lib::Bytes;
 use libc;
 
 #[derive(Debug, Copy, Clone)]
 pub struct Measured {
-    runtime_secs: f64,
-    memory_bytes: usize,
+    pub runtime: Duration,
+    pub memory: Bytes,
 }
 
-pub fn measure<F>(mut f: F, initial_mem: usize) -> Measured
+pub fn measure<F, T>(f: F) -> (Measured, T)
 where
-    F: FnMut(),
+    F: FnOnce() -> T,
 {
+    let initial_mem = get_maxrss();
     let start_time = Instant::now();
 
-    f();
+    let t = f();
 
-    Measured {
-        runtime_secs: start_time.elapsed().as_secs_f64(),
-        memory_bytes: get_maxrss().saturating_sub(initial_mem),
-    }
+    (
+        Measured {
+            runtime: start_time.elapsed(),
+            memory: get_maxrss().saturating_sub(initial_mem),
+        },
+        t,
+    )
 }
 
-pub fn get_maxrss() -> usize {
+/// Returns the maximum resident set size, ie the physical memory the program
+/// uses, in bytes.
+pub fn get_maxrss() -> Bytes {
     let rusage = unsafe {
         let mut rusage = std::mem::MaybeUninit::uninit();
         libc::getrusage(libc::RUSAGE_SELF, rusage.as_mut_ptr());
         rusage.assume_init()
     };
-    let mut maxrss = rusage.ru_maxrss as usize;
+    let maxrss = rusage.ru_maxrss as _;
     if cfg!(target_os = "macos") {
         // maxrss is in bytes
-        maxrss *= 1;
+        maxrss
     } else {
         // maxrss is in kilobytes
-        maxrss *= 1024;
+        maxrss * 1024
     }
-    maxrss
 }
 
-pub fn set_limits(time_secs: usize, mem_kb: usize) {
+pub fn set_limits(time: Duration, mem: Bytes) {
     let set = |res, limit| {
         let rlimit = libc::rlimit {
             rlim_cur: limit as _,
@@ -49,6 +55,7 @@ pub fn set_limits(time_secs: usize, mem_kb: usize) {
             libc::setrlimit(res, &rlimit);
         }
     };
-    set(libc::RLIMIT_CPU, time_secs);
-    set(libc::RLIMIT_AS, mem_kb);
+    set(libc::RLIMIT_CPU, time.as_secs());
+    // TODO(ragnar): Should this be in bytes for macos?
+    set(libc::RLIMIT_AS, mem / 1024);
 }
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,13 @@ mod bench;
 use crate::bench::*;
 
 use core_lib::*;
+use itertools::Itertools;
 
-use std::io::{self, prelude::*};
+use std::{
+    fs,
+    io::{self, prelude::*},
+    time::Duration,
+};
 
 use serde_json;
 
@@ -12,26 +17,58 @@ use clap::Parser;
 #[derive(Debug, Parser)]
 #[command(author, version, about)]
 struct Args {
-    #[arg(short, long, default_value_t = 3600usize)]
-    time_limit_secs: usize,
-    #[arg(short, long, default_value_t = 1048576usize)]
-    mem_limit_kb: usize,
+    #[arg(short, long, value_parser = parse_duration::parse, default_value = "1h")]
+    time_limit: Duration,
+    #[arg(short, long, value_parser = parse_bytes, default_value = "1GiB")]
+    mem_limit: Bytes,
 }
 
 fn main() {
     let args = Args::parse();
-    set_limits(args.time_limit_secs, args.mem_limit_kb);
+    set_limits(args.time_limit, args.mem_limit);
 
-    let mut input = String::new();
+    let mut stdin_job = vec![];
     io::stdin()
-        .read_to_string(&mut input)
+        .read_to_end(&mut stdin_job)
         .expect("Error in reading from stdin!");
-    let params: Parameters = serde_json::from_str(&input).expect("Error in parsing input json!");
+    let job: Job = serde_json::from_slice(&stdin_job).expect("Error in parsing input json!");
 
-    // TODO: read dataset
+    assert_eq!(
+        job.dataset
+            .extension()
+            .expect("Dataset does not have a file extension"),
+        "seq",
+        "Job dataset {} does not have extension .seq.",
+        job.dataset.display()
+    );
 
-    // make sure initial memory usage is measured after reading in the dataset
-    let initial_mem = get_maxrss();
-
-    // TODO: run the correct algorithm based on params
+    // NOTE: Although we could read and process the pairs in the dataset in streaming manner, that complicates the time and memory measurement.
+    let dataset = fs::read(&job.dataset).expect("Could not read dataset file");
+    let sequence_pairs: Vec<(Seq, Seq)> = dataset
+        .split(|&c| c == '\n' as u8)
+        .tuples()
+        .map(|(a, b)| {
+            (
+                a.strip_prefix(&['>' as u8])
+                    .expect("Odd lines must start with >"),
+                b.strip_prefix(&['<' as u8])
+                    .expect("Odd lines must start with >"),
+            )
+        })
+        .collect();
+    let (measured, (costs, cigars)) = measure(|| {
+        sequence_pairs
+            .into_iter()
+            .map(|(_a, _b)| -> (Cost, Cigar) {
+                todo!("Align the sequences a and b using the algorithm and parameters of choice.");
+            })
+            .unzip()
+    });
+    let output = JobOutput {
+        runtime: measured.runtime,
+        memory: measured.memory,
+        costs,
+        cigars,
+    };
+    println!("{}", serde_json::to_string(&output).unwrap());
 }
