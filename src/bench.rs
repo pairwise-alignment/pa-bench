bc3fae4fbd58f19c88b0919e0bbc5787f9131663
c0deb0t <daniel.liu02@gmail.com>
2022-12-07 16:12:40 -0800

    More work on setting up core features

--- src/bench.rs
+++ src/bench.rs
@@ -1,12 +1,17 @@
 use std::time::Instant;
 
+use libc;
+
+#[derive(Debug, Copy, Clone)]
 pub struct Measured {
     runtime_secs: f64,
     memory_bytes: usize,
 }
 
-pub fn measure<F>(f: F) -> (f64, usize) where F: FnMut() {
-    let initial_mem = get_maxrss();
+pub fn measure<F>(mut f: F, initial_mem: usize) -> Measured
+where
+    F: FnMut(),
+{
     let start_time = Instant::now();
 
     f();
@@ -17,7 +22,7 @@ pub fn measure<F>(f: F) -> (f64, usize) where F: FnMut() {
     }
 }
 
-fn get_maxrss() -> usize {
+pub fn get_maxrss() -> usize {
     let rusage = unsafe {
         let mut rusage = std::mem::MaybeUninit::uninit();
         libc::getrusage(libc::RUSAGE_SELF, rusage.as_mut_ptr());
@@ -32,4 +37,18 @@ fn get_maxrss() -> usize {
         maxrss *= 1024;
     }
     maxrss
+}
+
+pub fn set_limits(time_secs: usize, mem_kb: usize) {
+    let set = |res, limit| {
+        let rlimit = libc::rlimit {
+            rlim_cur: limit as _,
+            rlim_max: limit as _,
+        };
+        unsafe {
+            libc::setrlimit(res, &rlimit);
+        }
+    };
+    set(libc::RLIMIT_CPU, time_secs);
+    set(libc::RLIMIT_AS, mem_kb);
 }
