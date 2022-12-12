0f36207b08e6b7d91eb3431dd2004a9e7e27f8a0
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-12-12 15:21:16 +0100

    Add basic data generation to orchestrator

--- pa-bench-types/src/algorithms.rs
+++ pa-bench-types/src/algorithms.rs
@@ -1,18 +1,18 @@
 use serde::{Deserialize, Serialize};
 
 /// Which algorithm to run and benchmark, along with algorithm-specific parameters.
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum AlgorithmParams {
     BlockAligner(BlockAlignerParams),
     ParasailStriped(ParasailStripedParams),
     // Add more algorithms here!
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct BlockAlignerParams {
     pub min_size: usize,
     pub max_size: usize,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct ParasailStripedParams;
