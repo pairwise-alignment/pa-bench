1ba316b4aab589239237eaa3cf6c3b1e9ec01b6a
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2023-01-10 21:26:25 +0100

    Put Parasailors behind a feature flag

--- runner/src/wrappers/parasail.rs
+++ runner/src/wrappers/parasail.rs
@@ -1,44 +1,72 @@
-use super::*;
+#[cfg(feature = "parasailors")]
+mod with_parasailors {
+    use super::super::*;
 
-use parasailors::*;
+    use parasailors::{global_alignment_score, Matrix, Profile};
 
-pub struct ParasailStriped {
-    matrix: Matrix,
-    gap_open: i32,
-    gap_extend: i32,
-    s: ScoreModel,
-}
+    pub struct ParasailStriped {
+        matrix: Matrix,
+        gap_open: i32,
+        gap_extend: i32,
+        s: ScoreModel,
+    }
 
-impl AlignerParams for ParasailStripedParams {
-    type Aligner = ParasailStriped;
+    impl AlignerParams for ParasailStripedParams {
+        type Aligner = ParasailStriped;
 
-    fn default(cm: CostModel, trace: bool, _max_len: usize) -> Self::Aligner {
-        assert!(!trace);
-        let s = ScoreModel::from_costs(cm);
-        Self::Aligner {
-            matrix: Matrix::create("ACGT", s.r#match as _, s.sub as _),
-            gap_open: -s.open - s.extend,
-            gap_extend: -s.extend,
-            s,
+        fn default(cm: CostModel, trace: bool, _max_len: usize) -> Self::Aligner {
+            assert!(!trace);
+            let s = ScoreModel::from_costs(cm);
+            Self::Aligner {
+                matrix: Matrix::create("ACGT", s.r#match as _, s.sub as _),
+                gap_open: -s.open - s.extend,
+                gap_extend: -s.extend,
+                s,
+            }
+        }
+
+        fn is_exact(&self) -> bool {
+            true
         }
     }
 
-    fn is_exact(&self) -> bool {
-        true
+    impl Aligner for ParasailStriped {
+        fn align(&mut self, a: Seq, b: Seq) -> (Cost, Option<Cigar>) {
+            let a_len = a.len();
+            let a = Profile::new(a, &self.matrix);
+            (
+                self.s.global_cost(
+                    global_alignment_score(&a, b, self.gap_open, self.gap_extend),
+                    a_len,
+                    b.len(),
+                ),
+                None,
+            )
+        }
     }
 }
 
-impl Aligner for ParasailStriped {
-    fn align(&mut self, a: Seq, b: Seq) -> (Cost, Option<Cigar>) {
-        let a_len = a.len();
-        let a = Profile::new(a, &self.matrix);
-        (
-            self.s.global_cost(
-                global_alignment_score(&a, b, self.gap_open, self.gap_extend),
-                a_len,
-                b.len(),
-            ),
-            None,
-        )
+#[cfg(not(feature = "parasailors"))]
+mod without_parasailors {
+    use super::super::*;
+    pub struct ParasailStriped;
+
+    impl AlignerParams for ParasailStripedParams {
+        type Aligner = ParasailStriped;
+
+        fn default(_cm: CostModel, trace: bool, _max_len: usize) -> Self::Aligner {
+            assert!(!trace);
+            ParasailStriped
+        }
+
+        fn is_exact(&self) -> bool {
+            true
+        }
+    }
+
+    impl Aligner for ParasailStriped {
+        fn align(&mut self, _a: Seq, _b: Seq) -> (Cost, Option<Cigar>) {
+            unimplemented!("Enable parasailors feature to use Parasail.");
+        }
     }
 }
