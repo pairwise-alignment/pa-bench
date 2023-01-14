4bcc24f6a26d784816b12017ed62623d64f6e3d7
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2023-01-14 19:11:10 +0100

    Update A*PA for simplified aligner implementation

--- runner/src/wrappers/astarpa.rs
+++ runner/src/wrappers/astarpa.rs
@@ -1,24 +1,14 @@
-use astar_pairwise_aligner::aligners::{
-    astar::{AstarAligner, AstarPAParams},
-    cigar::CigarElement,
-};
+use astar_pairwise_aligner::{visualizer::Visualizer, *};
 
 use super::*;
 
-pub struct AstarPA {
-    trace: bool,
-    aligner: Box<dyn AstarAligner>,
-}
+impl<V: Visualizer + 'static> AlignerParams for AstarPaParams<V> {
+    type Aligner = Box<dyn AstarPaAligner>;
 
-impl AlignerParams for AstarPAParams {
-    type Aligner = AstarPA;
-
-    fn new(&self, cm: CostModel, trace: bool, _max_len: usize) -> Self::Aligner {
+    fn new(&self, cm: CostModel, _trace: bool, _max_len: usize) -> Self::Aligner {
+        // The trace parameter is ignored since A*PA gives a trace 'for free' currently.
         assert!(cm.is_unit());
-        AstarPA {
-            trace,
-            aligner: self.aligner(),
-        }
+        self.aligner()
     }
 
     fn is_exact(&self) -> bool {
@@ -26,38 +16,9 @@ impl AlignerParams for AstarPAParams {
     }
 }
 
-impl Aligner for AstarPA {
+impl Aligner for Box<dyn AstarPaAligner> {
     fn align(&mut self, a: Seq, b: Seq) -> (Cost, Option<Cigar>) {
-        if self.trace {
-            let (cost, cigar) = self.aligner.align(a, b);
-            let cigar = Cigar {
-                ops: cigar
-                    .into_iter()
-                    .filter_map(|&CigarElement { command, length }| {
-                        Some(CigarElem {
-                            op: match command {
-                                astar_pairwise_aligner::aligners::cigar::CigarOp::Match => {
-                                    Some(CigarOp::Match)
-                                }
-                                astar_pairwise_aligner::aligners::cigar::CigarOp::Sub => {
-                                    Some(CigarOp::Sub)
-                                }
-                                astar_pairwise_aligner::aligners::cigar::CigarOp::Ins => {
-                                    Some(CigarOp::Ins)
-                                }
-                                astar_pairwise_aligner::aligners::cigar::CigarOp::Del => {
-                                    Some(CigarOp::Del)
-                                }
-                                _ => None,
-                            }?,
-                            cnt: length as _,
-                        })
-                    })
-                    .collect(),
-            };
-            (cost as _, Some(cigar))
-        } else {
-            (self.aligner.cost(a, b) as _, None)
-        }
+        let (cost, cigar) = AstarPaAligner::align(self.as_mut(), a, b).0;
+        (cost, Some(cigar))
     }
 }
