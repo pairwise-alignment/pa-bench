bc3a7c3615255efe874627d8fcccc161510aadbf
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2024-03-05 14:09:44 +0100

    A*PA2: collect timings and statistics

--- pa-wrapper/src/wrappers/astarpa2.rs
+++ pa-wrapper/src/wrappers/astarpa2.rs
@@ -3,7 +3,7 @@ pub use astarpa2::AstarPa2Params;
 use crate::*;
 
 impl AlignerParamsTrait for AstarPa2Params {
-    type Aligner = Box<dyn pa_types::Aligner>;
+    type Aligner = Box<dyn astarpa2::AstarPa2StatsAligner>;
 
     fn build(
         &self,
@@ -22,9 +22,53 @@ impl AlignerParamsTrait for AstarPa2Params {
     }
 }
 
-impl AlignerTrait for Box<dyn pa_types::Aligner> {
+impl AlignerTrait for Box<dyn astarpa2::AstarPa2StatsAligner> {
     fn align(&mut self, a: Seq, b: Seq) -> (Cost, Option<Cigar>, AlignerStats) {
-        let (cost, cigar) = pa_types::Aligner::align(self.as_mut(), a, b);
-        (cost, cigar, AlignerStats::default())
+        let (cost, cigar, stats) = astarpa2::AstarPa2StatsAligner::align(self.as_mut(), a, b);
+        let mut s = AlignerStats::default();
+        // block stats
+        s.insert("fmax_tries".into(), stats.f_max_tries as _);
+        s.insert("num_blocks".into(), stats.block_stats.num_blocks as _);
+        s.insert(
+            "num_incremental_blocks".into(),
+            stats.block_stats.num_incremental_blocks as _,
+        );
+        s.insert(
+            "computed_lanes".into(),
+            stats.block_stats.computed_lanes as _,
+        );
+        s.insert("unique_lanes".into(), stats.block_stats.unique_lanes as _);
+        // trace stats
+        s.insert(
+            "dt_trace_tries".into(),
+            stats.trace_stats.dt_trace_tries as _,
+        );
+        s.insert(
+            "dt_trace_success".into(),
+            stats.trace_stats.dt_trace_success as _,
+        );
+        s.insert(
+            "dt_trace_fallback".into(),
+            stats.trace_stats.dt_trace_fallback as _,
+        );
+        s.insert("fill_tries".into(), stats.trace_stats.fill_success as _);
+        s.insert("fill_success".into(), stats.trace_stats.fill_success as _);
+        s.insert("fill_fallback".into(), stats.trace_stats.fill_success as _);
+        // timing stats
+        s.insert("t_precomp".into(), stats.t_precomp.as_secs_f64());
+        s.insert("t_jrange".into(), stats.t_j_range.as_secs_f64());
+        s.insert("t_fixed_jrange".into(), stats.t_fixed_j_range.as_secs_f64());
+        s.insert("t_pruning".into(), stats.t_pruning.as_secs_f64());
+        s.insert(
+            "t_compute".into(),
+            stats.block_stats.t_compute.as_secs_f64(),
+        );
+        s.insert("t_trace_dt".into(), stats.trace_stats.t_dt.as_secs_f64());
+        s.insert(
+            "t_trace_fill".into(),
+            stats.trace_stats.t_fill.as_secs_f64(),
+        );
+
+        (cost, cigar, s)
     }
 }
